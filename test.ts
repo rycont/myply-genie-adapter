53a645e689988fe53cedd62261c81a0d24dce392
rycont <reactdev@kakao.com>
2022-01-16 21:21:43 +0900

    fin

---
+++ test.ts
@@ -0,0 +1,10 @@
+import { findSongId, generateURL, getPlaylistContent } from "."
+
+getPlaylistContent("http://genie.co.kr/DCC7Z8")
+  .then(generateURL)
+  .then(console.log)
+// findSongId({
+//   artist: "세븐틴",
+//   channelIds: {},
+//   name: "Wave",
+// }).then(console.log)
